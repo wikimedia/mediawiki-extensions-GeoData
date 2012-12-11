30953f6c40ab4668b4ae46ac6e8d74a1c8b44470
Max Semenik <maxsem.wiki@gmail.com>
2012-12-11 18:39:16 +0400

    Re-throw Solr exceptions as MWException to avoid fatals
    Change-Id: I3ed15d40c9d3d1107ebdfdebb3fa5bf036d1ec55

--- api/ApiQueryGeoSearchSolr.php
+++ api/ApiQueryGeoSearchSolr.php
@@ -12,89 +12,93 @@ class ApiQueryGeoSearchSolr extends ApiQueryGeoSearch {
 		wfProfileIn( __METHOD__ );
 		parent::run( $resultPageSet );
 
-		$params = $this->extractRequestParams();
+		try {
+			$params = $this->extractRequestParams();
 
-		$solr = SolrGeoData::newClient();
-		$query = $solr->createSelect();
-		$helper = $query->getHelper();
+			$solr = SolrGeoData::newClient();
+			$query = $solr->createSelect();
+			$helper = $query->getHelper();
 
-		// @todo: props
-		$query->setQueryDefaultOperator( 'AND' );
-		$query->createFilterQuery( 'wiki' )->setQuery( 'wiki:' . wfWikiID() ); // Only Earth is supported
-		$query->createFilterQuery( 'globe' )->setQuery( 'globe:earth' ); // Only Earth is supported
-		if ( isset( $params['maxdim'] ) ) {
-			$query->addFilterQuery( "dim:[* TO {$params['maxdim']}]" );
-		}
-		$primary = array_flip( $params['primary'] );
-		if ( isset( $primary['yes'] ) != isset( $primary['no'] ) ) {
-			$query->createFilterQuery( 'primary' )->setQuery( 'primary:' . intval( isset( $primary['yes'] ) ) );
-		}
-		$query->createFilterQuery( 'coord' )->setQuery( $helper->geofilt( $this->lat, $this->lon, 'coord', $this->radius / 1000 ) );
-		$query->addSort( $helper->geodist( $this->lat, $this->lon, 'coord' ), Solarium_Query_Select::SORT_ASC );
-
-		$limit = $params['limit'];
-		$query->setRows( $limit + ( $this->idToExclude ? 1 : 0 ) ); // +1 in case we need to exclude a page
-
-		wfProfileIn( __METHOD__ . '-solr' );
-		$docs = $solr->select( $query );
-		wfProfileOut( __METHOD__ . '-solr' );
-		$mapping = array();
-		foreach ( $docs as $doc ) {
-			$id = $doc->page_id;
-			if ( !isset( $mapping[$id] ) && $id != $this->idToExclude ) {
-				$mapping[$id] = $doc;
+			// @todo: props
+			$query->setQueryDefaultOperator( 'AND' );
+			$query->createFilterQuery( 'wiki' )->setQuery( 'wiki:' . wfWikiID() ); // Only Earth is supported
+			$query->createFilterQuery( 'globe' )->setQuery( 'globe:earth' ); // Only Earth is supported
+			if ( isset( $params['maxdim'] ) ) {
+				$query->addFilterQuery( "dim:[* TO {$params['maxdim']}]" );
 			}
-		}
-
-		if ( !count( $mapping ) ) {
-			wfProfileOut( __METHOD__ );
-			return; // No results, no point in doing anything else
-		}
-		$this->addWhere( array( 'page_id' => array_keys( $mapping ) ) );
-
-		wfProfileIn( __METHOD__ . '-sql' );
-		$res = $this->select( __METHOD__ );
-		wfProfileOut( __METHOD__ . '-sql' );
-
-		$result = $this->getResult();
-		$rows = array();
-		foreach ( $res as $row ) {
-			$rows[$row->page_id] = $row;
-		}
-
-		foreach ( $mapping as $id => $doc ) {
-			if ( !$limit-- ) {
-				break;
+			$primary = array_flip( $params['primary'] );
+			if ( isset( $primary['yes'] ) != isset( $primary['no'] ) ) {
+				$query->createFilterQuery( 'primary' )->setQuery( 'primary:' . intval( isset( $primary['yes'] ) ) );
 			}
-			if ( !isset( $rows[$id] ) ) {
-				continue;
-			}
-			$row = $rows[$id];
-			if ( is_null( $resultPageSet ) ) {
-				$title = Title::newFromRow( $row );
-				list( $lat, $lon ) = explode( ',', $doc->coord );
-				$vals = array(
-					'pageid' => intval( $row->page_id ),
-					'ns' => intval( $title->getNamespace() ),
-					'title' => $title->getPrefixedText(),
-					'lat' => floatval( $lat ),
-					'lon' => floatval( $lon ),
-					'dist' => round( GeoDataMath::distance( $lat, $lon, $this->lat, $this->lon ), 1 ),
-				);
+			$query->createFilterQuery( 'coord' )->setQuery( $helper->geofilt( $this->lat, $this->lon, 'coord', $this->radius / 1000 ) );
+			$query->addSort( $helper->geodist( $this->lat, $this->lon, 'coord' ), Solarium_Query_Select::SORT_ASC );
 
-				if ( $doc->primary ) {
-					$vals['primary'] = '';
+			$limit = $params['limit'];
+			$query->setRows( $limit + ( $this->idToExclude ? 1 : 0 ) ); // +1 in case we need to exclude a page
+
+			wfProfileIn( __METHOD__ . '-solr' );
+			$docs = $solr->select( $query );
+			wfProfileOut( __METHOD__ . '-solr' );
+			$mapping = array();
+			foreach ( $docs as $doc ) {
+				$id = $doc->page_id;
+				if ( !isset( $mapping[$id] ) && $id != $this->idToExclude ) {
+					$mapping[$id] = $doc;
 				}
-				foreach( $params['prop'] as $prop ) {
-					$vals[$prop] = $doc->$prop;
-				}
-				$fit = $result->addValue( array( 'query', $this->getModuleName() ), null, $vals );
-				if ( !$fit ) {
+			}
+
+			if ( !count( $mapping ) ) {
+				wfProfileOut( __METHOD__ );
+				return; // No results, no point in doing anything else
+			}
+			$this->addWhere( array( 'page_id' => array_keys( $mapping ) ) );
+
+			wfProfileIn( __METHOD__ . '-sql' );
+			$res = $this->select( __METHOD__ );
+			wfProfileOut( __METHOD__ . '-sql' );
+
+			$result = $this->getResult();
+			$rows = array();
+			foreach ( $res as $row ) {
+				$rows[$row->page_id] = $row;
+			}
+
+			foreach ( $mapping as $id => $doc ) {
+				if ( !$limit-- ) {
 					break;
 				}
-			} else {
-				$resultPageSet->processDbRow( $row );
+				if ( !isset( $rows[$id] ) ) {
+					continue;
+				}
+				$row = $rows[$id];
+				if ( is_null( $resultPageSet ) ) {
+					$title = Title::newFromRow( $row );
+					list( $lat, $lon ) = explode( ',', $doc->coord );
+					$vals = array(
+						'pageid' => intval( $row->page_id ),
+						'ns' => intval( $title->getNamespace() ),
+						'title' => $title->getPrefixedText(),
+						'lat' => floatval( $lat ),
+						'lon' => floatval( $lon ),
+						'dist' => round( GeoDataMath::distance( $lat, $lon, $this->lat, $this->lon ), 1 ),
+					);
+
+					if ( $doc->primary ) {
+						$vals['primary'] = '';
+					}
+					foreach( $params['prop'] as $prop ) {
+						$vals[$prop] = $doc->$prop;
+					}
+					$fit = $result->addValue( array( 'query', $this->getModuleName() ), null, $vals );
+					if ( !$fit ) {
+						break;
+					}
+				} else {
+					$resultPageSet->processDbRow( $row );
+				}
 			}
+		} catch ( Solarium_Exception $e ) {
+			throw new MWException( get_class( $e ) . " at {$e->getFile()}, line {$e->getLine()}: {$e->getMessage()}", 0, $e );
 		}
 		wfProfileOut( __METHOD__ );
 	}
