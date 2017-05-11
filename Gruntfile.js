8fa08358c4c60584a795f05721dab59f3af6ba0c
Ed Sanders <esanders@wikimedia.org>
2017-05-11 15:41:26 +0100

    build: Replace jshint with eslint
    Change-Id: I7bde9f32e1bac4cb8d6656c5436d8d102b009f42

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,15 @@
-/*jshint node:true */
+/* eslint-env node, es6 */
+
 module.exports = function ( grunt ) {
 	var conf = grunt.file.readJSON( 'extension.json' );
 
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 
 	grunt.initConfig( {
 		banana: conf.MessagesDirs,
-		jshint: {
+		eslint: {
 			all: [
 				'**/*.js',
 				'!node_modules/**',
@@ -23,6 +24,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana', 'jshint' ] );
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana', 'eslint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
