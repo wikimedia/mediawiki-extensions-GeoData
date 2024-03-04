9593963685135fc10eb49f1356e7b5e0cb7a4bcc
thiemowmde <thiemo.kreuz@wikimedia.de>
2024-03-04 09:44:39 +0100

    Make ParseCoordTest assertions more specific
    The upstream method assertStatusError makes it very easy to check
    for specific error messages.
    
    Change-Id: Ie010745d63676e2daf92c4fd8e062bb5358f4fb5

--- tests/phpunit/ParseCoordTest.php
+++ tests/phpunit/ParseCoordTest.php
@@ -19,7 +19,7 @@ class ParseCoordTest extends MediaWikiIntegrationTestCase {
 	 * @covers \GeoData\CoordinatesParserFunction::parseCoordinates
 	 * @dataProvider provideCases
 	 */
-	public function testParseCoordinates( $parts, $result, $globe = 'earth' ) {
+	public function testParseCoordinates( array $parts, $result, string $globe = 'earth' ) {
 		$formatted = '"' . implode( '|', $parts ) . '"';
 
 		/** @var CoordinatesParserFunction $function */
@@ -37,8 +37,8 @@ class ParseCoordTest extends MediaWikiIntegrationTestCase {
 
 		$s = $function->parseCoordinates( $parts, new Globe( $globe ) );
 
-		if ( $result === false ) {
-			$this->assertStatusNotGood( $s, "Parsing of $formatted was expected to fail" );
+		if ( is_string( $result ) ) {
+			$this->assertStatusError( $result, $s, "Parsing of $formatted was expected to fail" );
 		} else {
 			$this->assertStatusGood( $s, "Parsing of $formatted was expected to succeed, but it failed" );
 			$val = $s->getValue();
@@ -66,48 +66,48 @@ class ParseCoordTest extends MediaWikiIntegrationTestCase {
 				new Coord( -20.511111111111, 40.765277777778 ) ],
 			[ [ 20, 30, 40, 'N', 40, 45, 55, 'W' ],
 				new Coord( 20.511111111111, -40.765277777778 ) ],
-			[ [ 20, 'E', 30, 'W' ], false ],
-			[ [ 20, 'S', 30, 'N' ], false ],
-			[ [ -20, 'S', 30, 'E' ], false ],
-			[ [ 20, 'S', -30, 'W' ], false ],
+			[ [ 20, 'E', 30, 'W' ], 'geodata-bad-latitude' ],
+			[ [ 20, 'S', 30, 'N' ], 'geodata-bad-longitude' ],
+			[ [ -20, 'S', 30, 'E' ], 'geodata-bad-latitude' ],
+			[ [ 20, 'S', -30, 'W' ], 'geodata-bad-longitude' ],
 			// wrong number of parameters
-			[ [], false ],
-			[ [ 1 ], false ],
-			[ [ 1, 2, 3 ], false ],
-			[ [ 1, 2, 3, 4, 5 ], false ],
-			[ [ 1, 2, 3, 4, 5, 6, 7 ], false ],
-			[ [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ], false ],
-			[ [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ], false ],
+			[ [], 'geodata-bad-input' ],
+			[ [ 1 ], 'geodata-bad-input' ],
+			[ [ 1, 2, 3 ], 'geodata-bad-input' ],
+			[ [ 1, 2, 3, 4, 5 ], 'geodata-bad-input' ],
+			[ [ 1, 2, 3, 4, 5, 6, 7 ], 'geodata-bad-input' ],
+			[ [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ], 'geodata-bad-input' ],
+			[ [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ], 'geodata-bad-input' ],
 			// unbalanced NESW
-			[ [ 'N', 'E' ], false ],
-			[ [ 12, 'N', 'E' ], false ],
-			[ [ 'N', 15, 'E' ], false ],
-			[ [ 1, 2, 3, 'N', 1, 'E' ], false ],
-			[ [ 1, 2, 3, 'N', 'E' ], false ],
-			[ [ 1, 2, 3, 'N', 1, 'E' ], false ],
-			[ [ 1, 2, 3, 'N', 1, 2, 'E' ], false ],
+			[ [ 'N', 'E' ], 'geodata-bad-latitude' ],
+			[ [ 12, 'N', 'E' ], 'geodata-bad-input' ],
+			[ [ 'N', 15, 'E' ], 'geodata-bad-input' ],
+			[ [ 1, 2, 3, 'N', 1, 'E' ], 'geodata-bad-longitude' ],
+			[ [ 1, 2, 3, 'N', 'E' ], 'geodata-bad-input' ],
+			[ [ 1, 2, 3, 'N', 1, 'E' ], 'geodata-bad-longitude' ],
+			[ [ 1, 2, 3, 'N', 1, 2, 'E' ], 'geodata-bad-input' ],
 			// Fractional numbers inconsistency
-			[ [ 1, 2.1, 3, 1, 2, 3 ], false ],
-			[ [ 1, 2.1, 3.2, 1, 2, 3 ], false ],
-			[ [ 1.00000001, 2.1, 3.2, 1, 2, 3 ], false ],
-			[ [ 1.00000001, 2.1, 3, 1, 2, 3 ], false ],
-			[ [ 1.00000001, 2, 3, 1, 2, 3 ], false ],
+			[ [ 1, 2.1, 3, 1, 2, 3 ], 'geodata-bad-latitude' ],
+			[ [ 1, 2.1, 3.2, 1, 2, 3 ], 'geodata-bad-latitude' ],
+			[ [ 1.00000001, 2.1, 3.2, 1, 2, 3 ], 'geodata-bad-latitude' ],
+			[ [ 1.00000001, 2.1, 3, 1, 2, 3 ], 'geodata-bad-latitude' ],
+			[ [ 1.00000001, 2, 3, 1, 2, 3 ], 'geodata-bad-latitude' ],
 			// only the last component of the coordinate should be non-integer
-			[ [ 10.5, 1, 20, 0 ], false ],
-			[ [ 10, 30.5, 1, 20, 0, 0 ], false ],
+			[ [ 10.5, 1, 20, 0 ], 'geodata-bad-latitude' ],
+			[ [ 10, 30.5, 1, 20, 0, 0 ], 'geodata-bad-latitude' ],
 			// Exception per https://phabricator.wikimedia.org/T50488
 			[ [ 1.5, 0, 2.5, 0 ], new Coord( 1.5, 2.5 ) ],
 			[ [ 1, 2.5, 0, 3, 4.5, 0 ], new Coord( 1.0416666666667, 3.075 ) ],
 			// coordinate validation (Earth)
 			[ [ -90, 180 ], new Coord( -90, 180 ) ],
-			[ [ 90.0000001, -180.00000001 ], false ],
-			[ [ 90, 1, 180, 0 ], false ],
-			[ [ 10, -1, 20, 0 ], false ],
-			[ [ 25, 60, 10, 0 ], false ],
-			[ [ 25, 0, 0, 10, 0, 60 ], false ],
+			[ [ 90.0000001, -180.00000001 ], 'geodata-bad-latitude' ],
+			[ [ 90, 1, 180, 0 ], 'geodata-bad-latitude' ],
+			[ [ 10, -1, 20, 0 ], 'geodata-bad-latitude' ],
+			[ [ 25, 60, 10, 0 ], 'geodata-bad-latitude' ],
+			[ [ 25, 0, 0, 10, 0, 60 ], 'geodata-bad-longitude' ],
 			// coordinate validation and normalisation (non-Earth)
 			[ [ 10, 20 ], new Coord( 10, 20, 'mars' ), 'mars' ],
-			[ [ 110, 20 ], false, 'mars' ],
+			[ [ 110, 20 ], 'geodata-bad-latitude', 'mars' ],
 			// Asimov Crater
 			[ [ 47, 0, 'S', 355, 3, 'W' ], new Coord( -47, 4.95, 'mars' ), 'mars' ],
 			// Quetzalpetlatl Corona
